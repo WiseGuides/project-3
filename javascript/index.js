f2df32bdf64b55a5b0283e6983bb17d17833fdac
Hal Helms <hal.helms@gmail.com>
2018-11-21 23:28:21 -0800

    21NOV2330

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,33 @@
-function details(element) {
-  alert(element);
+const programAuthors = 
+  {
+    eich: {
+    id: 'eich',
+    name: "Brendan Eich",
+    language: "JavaScript",
+    image: "eich.jpg",
+    dateOfBirth: "4 July 1961"
+  },
+  gosling: {
+    id: 'gosling',
+    name: "James Gosling",
+    language: "Java",
+    image: "gosling.jpg",
+    dateOfBirth: "19 May 1955"
+  },
+  hejlsberg: {
+    id: 'hejlsberg',
+    name: "Anders Hejlsberg",
+    language: "C#",
+    image: "hejlsberg.jpg",
+    dateOfBirth: "2 December 1960"
+  }
+};
+
+
+function details(id) {
+  let selectedProgramAuthor = programAuthors[id];
+  document.querySelector('.about h2').innerText = selectedProgramAuthor.name;
+  document.querySelector('.dob .info').innerText = selectedProgramAuthor.dateOfBirth;
+  document.querySelector('.language .info').innerText = selectedProgramAuthor.language;
+  document.querySelector('img').src = './images/' + selectedProgramAuthor.image;
 }
