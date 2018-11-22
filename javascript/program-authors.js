3e356fb03dbb6da8ce6c2739e0dd3148f19c5225
Hal Helms <hal.helms@gmail.com>
2018-11-21 17:27:50 -0800

    21NOV1730

---
+++ javascript/program-authors.js
@@ -0,0 +1,20 @@
+export const programAuthors = [
+  {
+    name: "Brendan Eich",
+    language: "JavaScript",
+    image: "eich.jpg",
+    dateOfBirth: "4 July 1961"
+  },
+  {
+    name: "James Gosling",
+    language: "Java",
+    image: "gosling.jpg",
+    dateOfBirth: "19 May 1955"
+  },
+  {
+    name: "Anders Hejlsberg",
+    language: "C#",
+    image: "hejlsberg.jpg",
+    dateOfBirth: "2 December 1960"
+  }
+];
